037c6e61013d97b57e3c6aa258352e156344c046
HernanSantos <xsantoshernan@gmail.com>
2022-09-12 00:34:09 -0300

    whothatpokemon

--- src/pokemons/pages/WhoThatPokemonPage.jsx
+++ src/pokemons/pages/WhoThatPokemonPage.jsx
@@ -7,24 +7,21 @@ import { useForm } from "../hooks/useForm";
 export const WhoThatPokemonPage = () => {
 
     const [whoPokemon, setWhoPokemon] = useState();
-    const [random, setRandom] = useState()
-    const [valorIngresado, setValorIngresado] = useState("")
+    const [random, setRandom] = useState("")
+    const [viewInput, setViewInput] = useState(false)
 
     const [valueForm,handleChange,reset] = useForm();
+    const [compare, setCompare] = useState();
 
     const randomPokemon = ()=>{
         const valorRandom = Math.floor(Math.random()*150)
         setRandom(valorRandom);
+        setCompare("")
+        setViewInput(true)
     } 
 
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-        setValorIngresado(valueForm.toLocaleLowerCase())
-        reset();
-    }
-
     useEffect(() => {
-        if (random !== undefined){
+        if (random.length !== 0){
             const dataPokemon = async()=>{
                 const {id,
                     name,
@@ -39,34 +36,50 @@ export const WhoThatPokemonPage = () => {
         }
     }, [random])
     
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        // setValorIngresado(valueForm.toLocaleLowerCase());
 
+        if(valueForm.toLocaleLowerCase() === whoPokemon.name){
+            setCompare(true)
+        }else{
+            setCompare(false)
+        }
+        reset();
+    }
+
+    
   return (
     <div className="search-container">
         <div className='text-center'>
             <Link
             to="/">
-                <img src="/assets/pokemon-logo.png" alt="pokemon-logo" />
+                <img src="/assets/pokemon-logo.png" alt="pokemon-logo"/>
             </Link>
       </div>
 
       <div>
 
-        <h2 className="text-center">¿Quien es este pokémon?</h2>
+        <h2 className= { `text-center" ${viewInput ? "view-input-revealed" :"view-input-hidden"}`}>¿Quien es este pokémon?</h2>
 
         <form onSubmit={handleSubmit}>
             <input type="text"
                     placeholder="ingresar nombre"
                     value={valueForm}
-                    onChange={handleChange} 
+                    onChange={handleChange}
+                    className={viewInput ?"view-input-revealed" :"view-input-hidden"}
             />
         </form>
 
         <div className="whoPokemonCard">
-            {(whoPokemon?.name === valorIngresado)
-                ? <div>{whoPokemon.name} {whoPokemon.id}</div>
-                :   ""
+            {(compare) && <div>{whoPokemon?.name}</div>
             }
-            {whoPokemon?.front_default && <img src={whoPokemon.front_default} alt="pokemon" className="who-img"/>}
+
+            {whoPokemon?.front_default && 
+                <img src={whoPokemon.front_default} 
+                    alt="pokemon" 
+                    className={(compare)? "who-img-revealed" : "who-img-hidden"}/>
+                    }
         </div>
         
         <button onClick={randomPokemon}>
