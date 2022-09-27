53111a08df418458e97f516300d2a717f58c6e14
HernanSantos <xsantoshernan@gmail.com>
2022-09-27 16:30:14 -0300

    loading

--- src/pokemons/components/Loading.jsx
+++ src/pokemons/components/Loading.jsx
@@ -3,7 +3,9 @@ export const Loading = () => {
 
   return (
     <div className="div-loading">
-        <img src="https://res.cloudinary.com/dtfmesfi0/image/upload/v1664243693/pokemon-app/fff2w1zvrtkl9yvezu6g.png" alt="loading" />
+        <img src="https://res.cloudinary.com/dtfmesfi0/image/upload/v1664243693/pokemon-app/fff2w1zvrtkl9yvezu6g.png" 
+              alt="loading"
+              className="img-loading" />
         <span className="loading">Loading...</span>
     </div>
   )
